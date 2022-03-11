f2c448a1cbd85ee36c207a47151b83fccd707db3
lukas <lukasglomb01@gmail.com>
2022-03-11 23:44:45 +0100

    Created Playground

---
+++ src/Schiffeversenken/field.java
@@ -0,0 +1,47 @@
+package Schiffeversenken;
+
+import java.util.ArrayList;
+
+public class field {
+    public static void main(String[] args) {
+
+
+        String[] letters = new String[11];
+
+        letters[0] = ("A");
+        letters[1] = ("B");
+        letters[2] = ("C");
+        letters[3] = ("D");
+        letters[4] = ("E");
+        letters[5] = ("F");
+        letters[6] = ("G");
+        letters[7] = ("H");
+        letters[8] = ("I");
+        letters[9] = ("J");
+        letters[10] = ("");
+
+
+
+
+        {
+            for (int i = 0; i < 11; i++) {
+                System.out.printf("\n" + letters[i]);
+
+            }
+
+            {
+                for (int i = 0; i < 11; i++) {
+                    System.out.printf("%2d",i);
+
+                }
+
+
+            }
+
+        }
+    }
+}
+
+
+
+
